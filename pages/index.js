a7300d229319c7de24bee28058b28898135da18f
“devjatin” <adriano23012001@gmail.com>
2023-03-23 10:44:24 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,61 @@
 import React, { Component } from 'react';
 
 class StartPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            email: '',
+            phone: '',
+            success: false
+        }
+    }
 
     moveToTop() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
 
-    validateEmail(mail) 
-    {
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
-        return (true)
-      }
-      alert("You have entered an invalid email address!")
-      return (false)
+    validateEmail(mail) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+            return (true)
+        }
+        alert("You have entered an invalid email address!")
+        return (false)
     }
 
     submitForm = () => {
-      let name, email, phone
-      name = document.querySelector('#name').value.trim()
-      email = document.querySelector('#email').value.trim()
-      phone = document.querySelector('#phone').value.trim()
+        let name, email, phone
+        name = this.state.name.trim()
+        email = this.state.email.trim()
+        phone = this.state.phone.trim()
 
-      console.log(name == "")
-      if(name == ""){
-        alert('plese enter your name')
-        return
-      }
+        if (name == "") {
+            alert('plese enter your name')
+            return
+        }
 
-      if(!this.validateEmail(email))
-      return
-    //   if (this.state.emails.find(email => email === this.email.value)) {
-    //     alert("Account with this email altready exists");
-    //     return;
-    // }
-    // if (this.password.value !== this.confirmPassword.value) {
-    //     alert("Passwords didn't match");
-    //     return;
-    // }
-    let newUserData = JSON.stringify({
-        "email": email,
-        "name": name,
-        "phone": Number(phone)
-    });
-      let myHeaders = new Headers();
+        if (!this.validateEmail(email))
+            return
+
+        if (phone == "") {
+            alert('plese enter your Mobile Number')
+            return
+        }
+        //   if (this.state.emails.find(email => email === this.email.value)) {
+        //     alert("Account with this email altready exists");
+        //     return;
+        // }
+        // if (this.password.value !== this.confirmPassword.value) {
+        //     alert("Passwords didn't match");
+        //     return;
+        // }
+        let newUserData = JSON.stringify({
+            "email": email,
+            "name": name,
+            "phone": Number(phone)
+        });
+        let myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         let requestOptions = {
             method: 'POST',
@@ -57,30 +69,42 @@ class StartPage extends Component {
             .then(response => {
                 let result = JSON.parse(response);
                 if (result.statusCode === 200) {
-                    alert('success')
+                    this.setState({ success: true })
+                    setTimeout(() => { this.setState({ success: false }) }, 5000)
                 }
             })
             .catch(error => {
                 alert("Account Creation Failed");
                 console.log('error', error)
             });
-    } 
+    }
+
+    handleName = (e) => {
+        this.setState({ name: e.target.value })
+    }
+
+    handleEmail = (e) => {
+        this.setState({ email: e.target.value })
+    }
+
+    handlePhone = (e) => {
+        this.setState({ phone: e.target.value })
+    }
 
     render() {
         return (<div>
-            <div className="mainpage" id='start_page'>
-                <div id="title"><h1>Want to learn to Code?</h1></div>
+            {this.state.success ? <div className="mainpage" id='start_page'> <div id="title"><h1>Congratulations, registeration succesful</h1></div> </div> : <div className="mainpage" id='start_page'>
+                <div id="title"><h1>Want to learn to code?</h1></div>
                 <div id="subtitle"><p>Learn to Code with Our Mentorship Programme</p></div>
                 <div id="main">
-                    {/* {window.screen.width < 768 && <SignUp signUpApis={this.props.signUpApis} getLessonProgressEmailAndUserName={this.props.getLessonProgressEmailAndUserName} />} */}
                     <div id="signup-mobile">
                         <div id="signupBody">
                             <div className="title">Join the waitlist now</div>
                             <div className='add-form'>
-                              <input type="text" id="name" placeholder="Your Name" required />
-                              <input type="email" id="email" name="email" placeholder='Enter your Email' required />
-                              <input type="phone" id="phone" name="phone" placeholder='Enter your Mobile Number' required />
-                              <button onClick={this.submitForm}>Submit</button>
+                                <input type="text" name='name' placeholder="Your Name" value={this.state.name} onChange={this.handleName} />
+                                <input type="email" name='email' placeholder='Enter your Email' value={this.state.email} onChange={this.handleEmail} />
+                                <input type="phone" name='phone' placeholder='Enter your Mobile Number' value={this.state.phone} onChange={this.handlePhone} />
+                                <button onClick={this.submitForm}>Submit</button>
                             </div>
                         </div>
                     </div>
@@ -95,7 +119,7 @@ class StartPage extends Component {
                             <ul>
                                 <li>Daily Mentorship</li>
                                 <li>One-on-one Guidance</li>
-                                <li>Earn Cashbacks while learning</li>
+                                <li>Earn CashRewards while learning</li>
                                 <li>Learn while doing live projects</li>
                             </ul>
                         </div>
@@ -104,15 +128,15 @@ class StartPage extends Component {
                         <div id="signupBody">
                             <div className="title">Join the waitlist now</div>
                             <div className='add-form'>
-                              <input type="text" id="name" placeholder="Your Name" required />
-                              <input type="email" id="email" name="email" placeholder='Enter your Email' required />
-                              <input type="phone" id="phone" name="phone" placeholder='Enter your Mobile Number' required />
-                              <button onClick={this.submitForm}>Submit</button>
+                                <input type="text" name='name' placeholder="Your Name" value={this.state.name} onChange={this.handleName} />
+                                <input type="email" name='email' placeholder='Enter your Email' value={this.state.email} onChange={this.handleEmail} />
+                                <input type="phone" name='phone' placeholder='Enter your Mobile Number' value={this.state.phone} onChange={this.handlePhone} />
+                                <button onClick={this.submitForm}>Submit</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>}
         </div>)
     }
 }
